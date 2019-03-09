1bfff3ca70d6f004e8ac38aaf44501cc8c86f59c
Frol8612 <infern00730@gmail.com>
2019-03-09 20:15:45 +0300

    feat: add responsive

---
+++ js/script.js
@@ -0,0 +1,29 @@
+(function() {
+  const btn = document.querySelector('#btn-menu');
+  const nav = document.querySelector('#nav-menu');
+
+  const delay = (ms) => new Promise(res => setTimeout(res, ms));
+
+
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+    delay(300).then(() => {
+      nav.classList.toggle('show');
+    });
+  });
+
+  document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (btn.classList.contains('active') && target !== btn && target.tagName !== 'UL' && target.className !== 'header__navbar') {
+      btn.classList.remove('active');
+      delay(300).then(() => {
+        nav.classList.remove('show');
+      });
+    }
+
+    /* if (target.tagName === 'A') {
+      window.scrollTo(target.id);
+    } */
+  });
+
+})();
