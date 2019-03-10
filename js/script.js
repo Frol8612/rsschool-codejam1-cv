41ba72e1d28103d8f9962ab57a107c013e656602
Frol8612 <infern00730@gmail.com>
2019-03-10 18:17:42 +0300

    fix: update responsive

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
 (function() {
   const btn = document.querySelector('#btn-menu');
   const nav = document.querySelector('#nav-menu');
+  const header = document.querySelector('header');
 
-  const delay = (ms) => new Promise(res => setTimeout(res, ms));
-
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
   btn.addEventListener('click', () => {
     btn.classList.toggle('active');
     delay(300).then(() => {
       nav.classList.toggle('show');
+      header.classList.toggle('show');
     });
   });
 
   document.addEventListener('click', (e) => {
     const target = e.target;
-    if (btn.classList.contains('active') && target !== btn && target.tagName !== 'UL' && target.className !== 'header__navbar') {
+    if (
+      btn.classList.contains('active') &&
+      target !== btn &&
+      target.tagName !== 'UL' &&
+      target.className !== 'header__navbar'
+    ) {
       btn.classList.remove('active');
       delay(300).then(() => {
         nav.classList.remove('show');
+        header.classList.remove('show');
       });
     }
   });
-
 })();
