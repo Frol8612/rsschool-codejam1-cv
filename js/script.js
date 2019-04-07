6f5add5016142a011e244cbf85386d27c34bacd0
Frol8612 <infern00730@gmail.com>
2019-04-07 19:48:35 +0300

    feat: add new styles for responsive

--- js/script.js
+++ js/script.js
@@ -1,38 +1,51 @@
-(function() {
+window.addEventListener('load', () => {
   const btn = document.querySelector('#btn-menu');
   const nav = document.querySelector('#nav-menu');
   const header = document.querySelector('header');
+
+  const arrElements = [nav, header];
+
   const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  const toggleClass = (elem, nameClass) => {
+    if(Array.isArray(elem)) {
+      elem.forEach(item => {
+        item.classList.toggle(nameClass);
+      })
+    } else {
+      elem.classList.toggle(nameClass);
+    }
+  }
 
   btn.addEventListener('click', () => {
-    btn.classList.toggle('active');
-    delay(300).then(() => {
-      nav.classList.toggle('show');
-      header.classList.toggle('show');
-    });
+    toggleClass(btn, 'active');
+    delay(300).then(toggleClass(arrElements, 'show-menu'));
   });
 
   document.addEventListener('click', (e) => {
-    const target = e.target;
+    const {
+      target,
+      target: { 
+        tagName,
+        className,
+        hash,
+      },
+    } = e;
 
     if (
       btn.classList.contains('active') &&
       target !== btn &&
-      target.tagName !== 'UL' &&
-      target.className !== 'header__navbar'
+      tagName !== 'UL' &&
+      className !== 'header__navbar'
     ) {
-      btn.classList.remove('active');
-      delay(300).then(() => {
-        nav.classList.remove('show');
-        header.classList.remove('show');
-      });
+      toggleClass(btn, 'active');
+      delay(300).then(toggleClass(arrElements, 'show-menu'));
     }
 
-    if (target.hash) {
+    if (hash) {
       e.preventDefault();
       return document
-        .getElementById(target.hash.slice(1))
-        .scrollIntoView({ behavior: 'smooth', block: "start" });
+        .getElementById(hash.slice(1))
+        .scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   });
 
@@ -40,11 +53,11 @@
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const scrollHeight = document.documentElement.clientHeight;
     const linkUp = document.querySelector('#link-up');
-  
-    if (scrollTop > scrollHeight ) {
-      linkUp.classList.add('show');
+
+    if (scrollTop > scrollHeight) {
+      linkUp.classList.add('show-link');
     } else {
-      linkUp.classList.remove('show')
+      linkUp.classList.remove('show-link');
     }
   });
-})();
+});
